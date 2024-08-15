e89e2ef4518f39bd947faf961fd88ed94bcbe7f9
Wagner Cardoso <wagner.c.dossantos@outlook.com>
2024-08-15 15:07:21 -0300

    Atualização 15-8-24

--- portfolio/javascript/main.js
+++ portfolio/javascript/main.js
@@ -9,5 +9,17 @@ theme.addEventListener('click', () => {
     else {
         this.textContent = 'Off';
     }
-    
+});
+
+document.getElementById('popupOpen1').addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById('popup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', function(){
+    document.getElementById('popup').style.display = 'none';
+});
+document.addEventListener('click', function(event) {
+    if (event.target === document.getElementById('popup')) {
+        document.getElementById('popup').style.display = 'none';
+    }
 });
